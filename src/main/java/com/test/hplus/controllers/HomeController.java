5576c111a3e2bbcd5ce750ff5d511c5704e8e723
Rushabh Shah <rushabh5000@gmail.com>
2021-11-21 10:53:03 +0530

    Adding initial project

---
+++ src/main/java/com/test/hplus/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.test.hplus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String goHome(){
+        System.out.println("in Home Controller");
+        return "index";
+    }
+
+    @GetMapping("/goToSearch")
+    public String goToSearch(){
+        System.out.println("going to search page");
+        return "search";
+    }
+}
